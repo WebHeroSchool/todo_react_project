396a1e680ea5693f8a0f5000193c266875de7ff6
Yulia <yulia.aksenova5@gmail.com>
2020-07-03 16:58:17 -0600

    todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const text = 'Some new words';
+var digits = 100;
+let moreText = 'That works!';
+let moreTextStill = 'Still Works!'
 function App() {
   return (
     <div className="App">
@@ -10,8 +13,18 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
+        <p style= {{
+          color: 'red',
+          fontSize: 12+24
+        }}>
         Hello World </p>
+        <p>  {text}, {digits}
+        </p>
+        <p> { digits>50 ? moreText : moreTextStill},
+         {digits===100 && ' And also this '+ moreTextStill}
+        </p>
+        <p> {null}, {false}, {undefined}, {true}
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
